cedb4e8f4682e788a4dcc6efeb0ceaff7b180b23
Jess Chadwick <jesschadwick@gmail.com>
2018-06-11 01:44:36 -0400

    Breaking 05_03 in twain

---
+++ Ch05/05_04_End/Common/Models/Image.cs
@@ -0,0 +1,13 @@
+﻿namespace HPlusSports.Models
+{
+    public class Image
+    {
+        public long Id { get; set; }
+
+        public string Url { get; set; }
+
+        public byte[] Content { get; set; }
+
+        public string ContentType { get; set; }
+    }
+}
