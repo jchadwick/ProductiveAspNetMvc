6aa520678345f1d583cc638744e489ee888e265e
Jess Chadwick <jesschadwick@gmail.com>
2018-06-11 22:49:05 -0400

    Re-recording 03_03

---
+++ Ch03/03_03_End/Website/Helpers/ProductHelpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using HPlusSports.Models;
+
+namespace HPlusSports
+{
+    public static class ProductHelpers
+    {
+        public static IHtmlString Rating(this System.Web.Mvc.HtmlHelper html, ProductRating rating)
+        {
+            if (rating == null || rating.ReviewCount == 0)
+            {
+                return new MvcHtmlString("<span><em>No Rating</em></span>");
+            }
+            return html.Partial("_Rating", rating);
+        }
+    }
+}
